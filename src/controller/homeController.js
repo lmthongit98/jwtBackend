acb685c6bd15592f72b5cb87916a22e1dbb90305
lmthongit98 <75925571+lmthongit98@users.noreply.github.com>
2022-03-20 15:51:07 +0700

    create new user

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,12 +1,42 @@
+// get the client
+import mysql from "mysql2";
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "123456",
+  database: "jwt",
+  port: 3306
+});
+
 const handleHelloWorld = (req, res) => {
-    return res.render("home");
-}
+  return res.render("home");
+};
 
 const handleUserPage = (req, res) => {
-    return res.render("user");
-}
+  return res.render("user");
+};
+
+const handleCreateNewUser = (req, res) => {
+  const {email, password, username} = req.body;
+
+  connection.query(
+    `INSERT INTO users (email, password, username) 
+     VALUES (?, ?, ?);`, [email, password, username],
+    function (err, results, fields) {
+     if(err){
+       console.log(err);
+     }else {
+       console.log(results);
+     }
+    }
+  );
+  return res.send("handleCreateNewUser");
+};
 
 module.exports = {
   handleHelloWorld,
   handleUserPage,
+  handleCreateNewUser,
 };
--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 import express from "express";
 import configViewEngine from './configs/viewEngine';
 import initWebRoutes from './routes/web';
+import bodyParser from 'body-parser';
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+// config body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Config view engine
 configViewEngine(app);
